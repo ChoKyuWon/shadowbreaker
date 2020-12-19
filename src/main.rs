4493df9042d95b4c20e3af379d15c3e18b05adb9
ChoKyuWon <kyuwoncho18@gmail.com>
2020-12-19 10:18:56 +0000

    found rust lib to support posix crypt(3)

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,29 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::char;
+
 // use std::error::Error;
 // use std::env; //for argv
-// use crypto::sha2::{Sha256, Sha512};
-// use crypto::digest::Digest;
-// use crypto::md5;
+use crypto::md5::Md5;
+use crypto::sha2::{Sha256, Sha512};
+use crypto::digest::Digest;
+
+use pwhash::unix::crypt;
 
 fn case_gen(len: usize) -> Vec<String>{
     let mut v : Vec<String> = Vec::new();
-    const start : char = 32 as char;
-    const end : char = 126 as char;
     if(len == 1){
-        for ch in 32..126{
+        for ch in 32..127{
             v.push(String::from(ch as u8 as char));
         }
         return v;
     }
-    for index in 2..len+1{
-        println!("Loop!");
-        let prev: String;
-        for ch in 32..126{
-            v.push("aa".to_string());
+    let prevs = case_gen(len - 1);
+    for prev in prevs{
+        for ch in 32..127{
+            let mut tmp_string:String = prev.to_owned();
+            tmp_string.push_str(&String::from(ch as u8 as char));
+            v.push(tmp_string);
         }
     }
     return v;
@@ -30,19 +32,60 @@ fn case_gen(len: usize) -> Vec<String>{
     //v.push(std::iter::repeat(start).take(len).collect::<String>());
 }
 
-fn md5_bruteforce(salt :&str, value:&str){
-    println!("{}, {}", salt, value);
+// fn md5_bruteforce(salt :&str, value:&str, case:&Vec<String>){
+//     let hashed_case = Vec::new();
+//     println!("{}, {}", salt, value);
+// }
+
+// fn sha256_bruteforce(salt :&str, value:&str, case:&Vec<String>){
+//     println!("{}, {}", salt, value);
+// }
+
+// fn sha512_bruteforce(salt :&str, value:&str, case:&Vec<String>){
+//     println!("{}, {}", salt, value);
+// }
+
+fn __bruteforce(func :&str, salt :&str, value:&str, cases :&Vec<String>){
+    let hashed_case: Vec<String> = Vec::new();
+    match func{
+        "1" => {
+            for case in cases{
+                let mut h = Md5::new();
+                h.input(case.as_bytes());
+                let mut out = [0; 16];
+                h.result(&mut out);
+                println!("Hashed value: {:?}, origin value: {}",out, case);
+            }
+        },
+        "5" => {
+            for case in cases{
+                let mut h = Sha256::new();
+                h.input(case.as_bytes());
+                let mut out = [0; 32];
+                h.result(&mut out);
+                println!("Hashed value: {:?}, origin value: {}",out, case);
+            }
+        },
+        "6" => {
+            for case in cases{
+                let mut h = Sha512::new();
+                h.input(case.as_bytes());
+                let mut out = [0; 64];
+                h.result(&mut out);
+                println!("Hashed value: {:?}, origin value: {}",out, case);
+            }
+        },
+        _ => println!("{}: This algorithm is not supported, sorry.", func),
+    }
 }
 
-fn sha256_bruteforce(salt :&str, value:&str){
-    println!("{}, {}", salt, value);
+fn bruteforce(salt: &str, cases :&Vec<String>){
+    for case in cases{
+        println!("{}", crypt(case, salt).ok().unwrap());
+    }
 }
-
-fn sha512_bruteforce(salt :&str, value:&str){
-    println!("{}, {}", salt, value);
-}
-
 fn main() {
+    let case = case_gen(1);
     let path = Path::new("shadow");
     let display = path.display();
 
@@ -66,13 +109,10 @@ fn main() {
             _ => v[1],
         };
         println!("[*]Username {}: crack start.", username);
-        let hashed: Vec<&str> = h.split('$').collect();
-        match hashed[1]{
-            "1" => md5_bruteforce(hashed[2], hashed[3]), //MD5
-            "5" => sha256_bruteforce(hashed[2], hashed[3]), //SHA256
-            "6" => sha512_bruteforce(hashed[2], hashed[3]), //SHA512
-            _ => println!("{}: This algorithm is not supported", v[0]),
-        }
+
+        bruteforce(h, &case);
+
+        // let hashed: Vec<&str> = h.split('$').collect();
+        // __bruteforce(hashed[1], hashed[2], hashed[3], &case);
     }
-    case_gen(2);
 }
